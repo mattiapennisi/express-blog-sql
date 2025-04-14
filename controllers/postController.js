75614481e6fd1b0b7301f1da423b2bf31bf714b2
mattiapennisi <mattiapennisi0@gmail.com>
2025-04-14 16:05:16 +0200

    Fix database connection import and implement index function to retrieve posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,12 @@
-const connection = require('../data/db')
+const connection = require('../data/db.js')
 
 function index(req, res) {
+    const sql = 'SELECT * FROM posts'
 
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+        res.json(results)
+    })
 }
 
 function show(req, res) {
